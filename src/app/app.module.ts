0475d4053995002eaf9b1a85d8b2601b58392767
Nipun Dineshan <1013504@questgedctvm.com>
2020-04-01 09:42:27 +0530

    added microfrontend navbar

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmptyRouteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FlexLayoutModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
